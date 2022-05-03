35490ad7c35db24222de3deaf90e8452b04debd5
Gabriela <s.gabriela.bv96@gmail.com>
2022-05-03 16:22:12 +0200

    hata la iteracion 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import contacts from "./contacts.json";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const contactsArr = contacts.splice(0, 5);
+
+  const [contactsList, setContactsList] = useState(contactsArr);
+
+  function newRandom() {
+
+
+    let contactsCopy = [...contactsList]
+  
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)]
+    contactsCopy.unshift(randomContact);
+
+    setContactsList(contactsCopy);
+  }
+
+
+
+
+  
+
+    return (
+        <div className="App">
+            <h1>IronContacts</h1>
+            <button onClick={() => newRandom()}> Add Random Contact </button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Pictire</th>
+                        <th>Name</th>
+                        <th>Popularity</th>
+                        <th>Won an Oscar</th>
+                        <th>Won an Emmy</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {contactsList.map((contact) => (
+                        <tr>
+                            <td>
+                                <img src={contact.pictureUrl} alt={contact.name} />
+                            </td>
+                            <td>{contact.name}</td>
+                            <td>{contact.popularity.toFixed(2)}</td>
+                            <td>{contact.wonOscar ? "🏆" : ""}</td>
+                            <td>{contact.wonEmmy ? "🏆" : ""}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
